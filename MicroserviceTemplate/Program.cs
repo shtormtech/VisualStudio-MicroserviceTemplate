d4a80f290602bf835355d9243180cd4fb3a42837
Иван Варнавский <ivan.varnavskiy@shtormtech.ru>
2020-08-17 08:26:15 +0300

    async main

--- MicroserviceTemplate/Program.cs
+++ MicroserviceTemplate/Program.cs
@@ -6,14 +6,16 @@ using App.Metrics.Formatters.Prometheus;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
+using System.Threading.Tasks;
+
 namespace $safeprojectname$
 {
     public class Program
     {
         public static IMetricsRoot Metrics { get; set; }
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
